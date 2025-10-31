b92d5aaba14087892adfd49e4f6f132ab501087f
vdao26 <vdao@terpmail.umd.edu>
2025-10-31 16:26:40 -0400

    Update DataCleaner.py

---
+++ src/DataCleaner.py
@@ -0,0 +1,17 @@
+class DataCleaner:
+     """The functions that will be in this class:
+    __init__(dataframe: pd.DataFrame)
+
+    clean_titles() -> pd.DataFrame
+
+    standardize_genres() -> pd.DataFrame
+
+    remove_duplicates() -> pd.DataFrame
+
+    fix_empty_genres() -> pd.DataFrame
+
+    clean_all() -> pd.DataFrame
+    
+    
+    
+    """
