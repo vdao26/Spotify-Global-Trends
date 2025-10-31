34e3bee2de198c1a34bd8ce8bb26f089c6217653
vdao26 <vdao@terpmail.umd.edu>
2025-10-31 16:26:44 -0400

    Update CVSManager.py

--- src/CVSManager.py
+++ src/CVSManager.py
@@ -1,3 +1,13 @@
 class CVSManager:
     """Handles loading, validating, and slicing of Spotify Top 50 data."""
+    """The functions that will be in this class:
     
+    __init__(csv_filename: str)
+
+    load_and_validate_csv() -> pd.DataFrame
+
+    count_tracks() -> int
+    
+    
+    
+    """
