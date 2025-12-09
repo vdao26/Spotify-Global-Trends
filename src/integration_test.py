16cc2981b150cbdae72bc81e4af8d1deb3182ad4
FatimahShaw <fshaw12@terpmail.umd.edu>
2025-12-09 09:00:15 -0500

    Update integration_test.py

--- src/integration_test.py
+++ src/integration_test.py
@@ -11,8 +11,8 @@ from song_item import SongItem
 class TestIntegration(unittest.TestCase):
 
   def test_csvmanager_in_datacleaner(self):
-    csv_manager = CSVManager("SpotifyTopSongs - May 2020.csv")
-    df csv_manager.load_and_validate_csv()
+    csv_manager = CSVManager("SpotifyTopSongsByCountry - May 2020.csv")
+    df = csv_manager.load_and_validate_csv()
     self.assertTrue(df is not None)
     self.assertTrue(type(df)== pd.DataFrame)
     self.assertGreater(len(df), 0)
@@ -20,8 +20,8 @@ class TestIntegration(unittest.TestCase):
     self.assertTrue("Artists" in df.columns)
     self.assertTrue("Genre" in df.columns)
     data_cleaner = DataCleaner(df)
-    cleaned_df = cleaner.clean_all()
+    cleaned_df = data_cleaner.clean_all()
     self.assertTrue(type(cleaned_df)==pd.DataFrame)
-    self.assertTrue(len(cleaned) > 0)
+    self.assertTrue(len(cleaned_df) > 0)
     self.assertTrue("Title" in cleaned_df.columns)
     self.assertTrue("Genre" in cleaned_df.columns)
