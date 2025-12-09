035a500d91b3df33f1145b392a26ce03fadab891
FatimahShaw <fshaw12@terpmail.umd.edu>
2025-12-09 08:27:29 -0500

    Update integration_test.py

--- src/integration_test.py
+++ src/integration_test.py
@@ -1,1 +1,12 @@
+import unittest
+import os
+import pandas as pd
+from CSVManager import CSVManager
+from DataCleaner import DataCleaner
+from DatabaseManagement import DatabaseManagement
+from MusicAnalyzer import MusicAnalyzer
+from artist_item import ArtistItem
+from song_item import SongItem
+
+
 
